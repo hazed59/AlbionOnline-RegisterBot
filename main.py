48109789178ff0f8dab1562e2cb9360a75bb6613
vinanrra <vicentefernandezguerra@gmail.com>
2021-09-15 13:00:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,37 +23,36 @@ client.run('your token here')
 @bot.command()
 async def register(ctx, arg):
     # await ctx.send(arg)
-    await
-      # Petición a la url, y guarda la respuesta
-      response = requests.get(url)
+    # Petición a la url, y guarda la respuesta
+    response = requests.get(url)
 
     # Devuelve la URL y el formato final no es un salto de linea, si no un espacio y se unen dos prints
     # Quedando -> https://gameinfo.albiononline.com/api/gameinfo/search?q=Queenmirna  200
     # print("{}".format(url), end =" ")
 
     if response.status_code == 200:
-      # Transforma la info en texto plano a json
-      data_json = json.loads(response.text)
-      exist = False
+        # Transforma la info en texto plano a json
+        data_json = json.loads(response.text)
+        exist = False
 
-      # Si players existe en data_json
-      if data_json.get('players'):
+        # Si players existe en data_json
+        if data_json.get('players'):
 
-          for player in data_json['players']:
-          
-              # Si player es igual al nombre del jugador pasado devolver datos
-              if player['Name'].lower() == arg.lower():
-                  exist = True
-                  # Falta añádir que le de el rol y le cambie el nombre
-                  # Añadir que le rol o nombre si es clan o alianza
+            for player in data_json['players']:
+            
+                # Si player es igual al nombre del jugador pasado devolver datos
+                if player['Name'].lower() == arg.lower():
+                    exist = True
+                    # Falta añádir que le de el rol y le cambie el nombre
+                    # Añadir que le rol o nombre si es clan o alianza
 
-          # Si no existe un jugador con ese nombre, puede que sea incompleto o existan varios, pero no coincide ninguno
-          if not exist:
-                  ctx.send("No existe el jugador {}, verifica que el nombre sea igual que en el juego.".format(arg))
-      
-      # No existe ningún jugador con ese nombre, NO devuelve resultados la lista
-      else:
-          ctx.send("No existe el jugador {}, verifica que el nombre sea igual que en el juego.".format(arg))
+            # Si no existe un jugador con ese nombre, puede que sea incompleto o existan varios, pero no coincide ninguno
+            if not exist:
+                    ctx.send("No existe el jugador {}, verifica que el nombre sea igual que en el juego.".format(arg))
+        
+        # No existe ningún jugador con ese nombre, NO devuelve resultados la lista
+        else:
+            ctx.send("No existe el jugador {}, verifica que el nombre sea igual que en el juego.".format(arg))
     
     # La API no está disponible
     else:
