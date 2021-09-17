6abed06d98c93ccf7c74a6e02e5e32b1bb0c9be6
Vicente Fernandez <e_vfer01@mercadona.com>
2021-09-17 13:29:42 +0200

    Save users on db

--- main.py
+++ main.py
@@ -417,6 +417,27 @@ async def register(ctx, username):
                     except nextcord.errors.Forbidden:
                         pass
 
+                    # Guardar la ID de la guild
+                    DiscordGuildID = ctx.message.guild.id
+                    # Nombre de las tablas según ID de la guild
+                    table_users = "registedUsers{}".format(DiscordGuildID)
+
+                    con = sqlite3.connect('example.db')
+
+                    # Create cursor
+                    cur = con.cursor()
+
+                    memberId = ctx.message.author.id
+                    # Se pone "f" delante para que se reconozca las {} como variables
+                    # Insertar datos en la tabla
+                    cur.execute(f"""INSERT INTO {table_users} (userid, albionnick) values (?, ?)""", (memberId, username))
+                    
+                    # Guardar cambios
+                    con.commit()
+
+                    # Cerrar conexión
+                    con.close()
+
                     break
                 
                 # INFO - Alliance
