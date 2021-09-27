f0347131ef1742ac7f124272425dcae7ef0cef3f
Vicente Fernandez <e_vfer01@mercadona.com>
2021-09-27 16:50:55 +0200

    Initial check + Fixed unregister permissions

--- cogs/check.py
+++ cogs/check.py
@@ -2,24 +2,19 @@ from discord.ext import commands
 import discord
 from datetime import datetime,timezone
 
-class utcCog(commands.Cog, name="utc Command"):
+class checkCog(commands.Cog, name="check Command"):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(
         pass_context=True,
-        brief="Get UTC time",
-        help="Get UTC time.\nExameple: !utc",
+        brief="Get user info",
+        help="Get user info (Guild, ).\nExameple: !check",
     )
-    async def utc(self, ctx):
-      
-        now_utc = datetime.now(timezone.utc).strftime("%H:%M:%S")
-
-        utcTime = discord.Embed(description="⏰ UTC time now: {}".format(now_utc))
-
-        await ctx.send(embed=utcTime)
+    async def check(self, ctx):
+      await ctx.send("Hi")
 
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
 # When we load the cog, we use the name of the file.
 def setup(bot):
-    bot.add_cog(utcCog(bot))
+    bot.add_cog(checkCog(bot))
